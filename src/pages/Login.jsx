00529a2f0f1530839100f902be506e428219f93e
alfiansaputra01 <alfiansp0106@gmail.com>
2023-05-26 21:24:28 +0700

    LATIHAN

---
+++ src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../redux/actions/auth";
+
+function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const {user, isLoggedin} = useSelector((state) => state.auth);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (isLoggedin || user) {
+      navigate("/");
+    }
+  }, [isLoggedin, navigate, user]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const data = { email, password };
+
+    dispatch(login(data, navigate));
+  };
+
+  return (
+    <Container className="p-4">
+      <Row>
+        <Col>
+          {" "}
+          <Form onSubmit={onSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Login;
